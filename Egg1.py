a373a291c4959fa0d7505a33d21f24ba6ce418dd
Osvaldo Ruiz <os.ruizh@duocuc.cl>
2026-04-23 15:21:00 -0400

    si
    un cambio

---
+++ Egg1.py
@@ -0,0 +1,13 @@
+grade1 = float(input("Ingrese su primera nota: "))
+grade2 = float(input("Ingrese su segunda nota: "))
+grade3 = float(input("Ingrese su tercera nota: "))
+
+ntotal = (grade1 * 0.3) + (grade2 * 0.3) + (grade3 * 0.4)
+if ntotal >=6.0:
+    print(f"Aprobó con distincioón, su nota es: {ntotal:.2f}")
+elif ntotal < 6.0 and ntotal >= 4.0:
+    print(f"Aprobó, su nota es: {ntotal:.2f}")
+elif ntotal < 4.0:
+    print(f"Reprobó, su nota es: {ntotal:.2f}")
+else:
+    print("Valor invalido")
