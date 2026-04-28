21ed1c3b4b537a7e8cda03a241bfad9944069920
Carlos Muñoz <caa.munozp@duocuc.cl>
2026-04-28 01:23:10 -0400

    Update electrica_CM_28_04.py

--- electrica_CM_28_04.py
+++ electrica_CM_28_04.py
@@ -2,8 +2,10 @@
 - residencial, consumo <= 150 kWh: $82 por kWh
 - residencial, consumo > 150 kWh: primeros 150 kWh a $82 y el exceso a $110
 - Comercial (cualquier consumo): $130 por kWh
+Mostrar el nombre del cliente en Mayusculas, su largo, tipo de tarifa, consumo y monto a pagar
 """
 
+name = input("Ingrese su nombre: ")
 consumo = float(input("Ingrese su consumo en kWh: "))
 tarifa = input("Ingrese tipo de cliente (residencial/comercial): ").lower()
 
@@ -12,8 +14,20 @@ if consumo > 150 and tarifa == "residencial":
     cobro = 150 * 82 + consumo_exceso * 110
 elif consumo <= 150 and tarifa == "residencial":
     cobro = consumo * 82
-else:
+elif tarifa == "comercial":
     cobro = consumo * 130
+else:
+    cobro = -1
+    
 
-print(f"Su factura es de: ${cobro}, ya que consumio {consumo} kWh")
+print(f"Nombre: {name.upper()}")
+print(f"Largo del nombre: {len(name)}")
+if cobro != -1:
+    print(f"Tipo de tarifa: {tarifa}")
+    print(f"Consumo: {consumo}")
+    print(f"Monto a pagar: {cobro}")
+else:
+    print(f"Tipo de tarifa: Incorrecta")
+    print(f"Consumo: {consumo}")
+    print(f"Monto a pagar: ----------- ")
 
