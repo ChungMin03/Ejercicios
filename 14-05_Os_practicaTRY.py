ac6c2b7d1c593d643cdb57e82685c95703103c27
Osvaldo Ruiz <os.ruizh@duocuc.cl>
2026-05-15 20:42:24 -0400

    practicas con listas y funciuones

--- 14-05_Os_practicaTRY.py
+++ 14-05_Os_practicaTRY.py
@@ -6,7 +6,7 @@
  la opcion 3 reinicia los contadores"""
 
 
-
+"""
 #-------------------Bloque de variables---------------------------------------------------------
 
 opcion = 0
@@ -23,18 +23,21 @@ muy_insatisfecho = 0
 
 #-------------------Bloque del menú----------------------------------------------------------------
 while opcion != 4:
+
     print("***Menu de Encuesta***")
     print("1.- Ingrese su nivel de satisfaccion (1-5)")
     print("2.- Ver estadisticas")
     print("3.- Nueva encuesta (Reinicia informacion anterior)")
     print("4.- Salir")
 
-    try:
-        opcion = int(input("Ingrese una de las opciones(1-4): "))
-    except ValueError:
-        print("Ingrese un valor numerico del 1 al 4")
+
 
 #-------------------Bloque de ejecución-------------------------------------------------------------
+    try:
+        opcion = int(input("Ingrese una de las opciones(1-4): "))
+
+    except ValueError:
+        print("Ingrese un valor numerico del 1 al 4")
 
     if opcion == 1:
         flag1 = False
@@ -45,6 +48,7 @@ while opcion != 4:
             except ValueError:
                 print("Ingrese un valor numerico")
 
+        #Sumando los niveles de satisfaccion
         total_respuestas += 1
         total_nivel += nivel
         if nivel == 5:
@@ -61,6 +65,7 @@ while opcion != 4:
             print("Valor invalido")
             total_respuestas -= 1
 
+        #Verificar cual e la mejor nota
         if muy_satisfecho != 0:
             puntaje = 5
         elif satisfecho != 0:
@@ -76,20 +81,51 @@ while opcion != 4:
     elif opcion == 2:
         if total_respuestas == 0:
             print("Aun no hay encuestas")
+
         else:
             print(f"La nota mas alta es {puntaje}/5")
             print(f"El total de encuestados es de {total_respuestas}")
             print(f"El promedio de puntaje es de {total_nivel/total_respuestas}")
             print(f"En promedio de pesrsonas satifechas y muy satisfechas es de {(muy_satisfecho+satisfecho)/total_respuestas}")
+
     elif opcion == 3:
         print("Encuestas reiniciadas")
         total_respuestas = 0
         puntaje = 0
+
         #valores de satisfaccion
+
         muy_satisfecho = 0
         satisfecho = 0
         neutro = 0
-        insatisfecho = 0
+        insatisfecho = 0    
         muy_insatisfecho = 0
     elif opcion == 4:
-        print("Gracias por usar nuestro programa de encuestas")
+
+        print("Gracias por usar nuestro programa de encuestas")"""
+
+
+
+#--------------intento con lista ---------------------
+#definimos variables
+lista_nivel = []
+flag1 = False
+
+#while para que no se apage la encuesta hasta que ingrese una respeusta valida
+while not flag1:
+    try:
+        nivel = int(input("Ingrese Su nivel de satisfacción\n5 = Muy satisfecho\n4 = Satisfecho\n3 = Neutral\n2 = Insatisfecho\n1 = Muy insatisfecho\n "))
+        if nivel > 5 or nivel < 1:
+            raise ValueError("El nivel de satisfaccion debe estar entre 1 y 5")
+    
+    except ValueError as error:
+        print(f"Error: {error}")
+        flag1 = True
+        top_nivel = lista_nivel.count(5) + lista_nivel.count(4)
+        
+    lista_nivel.append(nivel)
+#Prints de la lista
+print(len(lista_nivel)-1)
+print(max(lista_nivel))
+print(min(lista_nivel))
+print(f"{(top_nivel/(len(lista_nivel)-1))*100}%")
