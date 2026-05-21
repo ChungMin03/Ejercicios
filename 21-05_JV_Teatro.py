bad87020e416e4b98244dbd2c562444082f4d564
JavieraV <ja.varelav@duocuc.cl>
2026-05-21 19:48:22 -0400

    Update 21-05_JV_Teatro.py

--- 21-05_JV_Teatro.py
+++ 21-05_JV_Teatro.py
@@ -48,21 +48,28 @@ Opción 5 — Salir
 # ----------------- Bloque de variables -----------------
 historial_ventas = 0
 devoluciones = 0
+historial_total_ventas = 0
+
+# Esta variable global representa la matriz de asientos del teatro.
 matriz_asientos = [["*"] * 20 for _ in range(10)]
 
 # ----------------- Bloque funciones -----------------
 
-
+# Esta función permite reservar un asiento, seleccionando la fila y columna, remplazando el valor "*" por "X" en la matriz de asientos.
+# Si el asiento ya está ocupado, se muestra un mensaje indicando que no se puede reservar ese asiento.
+# La funcion retorna True si se utiliza. Esto debido a que al no retornar nada, el valor por defecto es None, lo que no permite incrementar el historial de ventas.
 def asiento_ocupado():
     ocupar_asiento = False
     while not ocupar_asiento:
         try:
             fila = int(input("Ingrese la fila (1-10): "))
             columna = int(input("Ingrese la columna (1-20): "))
-
+            
+            # Se valida que la fila y columna ingresada estén dentro del rango permitido (1-10 para filas y 1-20 para columnas)
             if fila < 1 or fila > 10 or columna < 1 or columna > 20:
                 print("Número de fila o columna fuera de rango. Por favor, ingrese valores válidos.")
             else:
+                # Se resta 1 a la fila y columna para ajustar a los índices de la matriz (que comienzan en 0)
                 if 0 <= (fila - 1) <= 9 and 0 <= (columna - 1) <= 19:
                     if matriz_asientos[(fila - 1)][(columna - 1)] == "X":
                         print("El asiento ya está ocupado.")
@@ -73,9 +80,13 @@ def asiento_ocupado():
 
         except ValueError:
             print("No ingreso un numero entero")
+    return True
 
 
-
+# Esta función permite devolver un asiento, seleccionando la fila y columna, remplazando el valor "X" por "*" en la matriz de asientos.
+# Si el asiento ya está disponible, se muestra un mensaje indicando que no se puede devolver ese asiento.
+# La funcion retorna True si se utiliza.
+# Esta función es similar a la función de reservar asiento, pero en lugar de marcar el asiento como ocupado ("X"), lo marca como disponible ("*").
 def devolver_asiento():
     devolver_asiento = False
     while not devolver_asiento:
@@ -96,10 +107,10 @@ def devolver_asiento():
             
         except ValueError:
             print("No ingreso un numero entero")
+    return True
 
 
 # ----------------- Bloque de ejecución -----------------
-
 menu = False
 
 while not menu:
@@ -109,6 +120,7 @@ while not menu:
     print("3.- Devolver localidades")
     print("4.- Historial de ventas")
     print("5.- Salir")
+    print("------------------------------------\n")
 
     # Validacion de ingreso de opcion del menu
     opc = False
@@ -120,26 +132,35 @@ while not menu:
             print("No ingreso una opción válida.")
         
     if opcion == 1:
-        print("Localidades disponibles:\n")
+        
+        print("\nLocalidades disponibles:")
         for fila in matriz_asientos:
-            print(" ".join(fila))
+            # Se utiliza el método join para convertir cada fila de la matriz de asientos en una cadena de texto, separando los elementos con dos espacios ("  ").
+            print("  ".join(fila))
 
     
     elif opcion == 2:
         ciclo = False
         while not ciclo:
             print("Ingrese el asiento que desea reservar")
+            # Se utiliza una comprensión de listas para contar el número de asientos ocupados ("X") en la matriz de asientos. 
+            # La función sum() se encarga de sumar los conteos de cada fila para obtener el total de asientos ocupados.
             ocupadas = sum(fila.count("X") for fila in matriz_asientos)
 
+            # Para evitar errores, se valida que existan asientos disponibles antes de intentar reservar un asiento. Si no hay asientos disponibles, el programa termina el ciclo.
             if ocupadas >= 200:
                 print("No hay localidades disponibles.")
+                print("Volviendo al menú principal...")
                 ciclo = True
             else:
+                # 
                 if asiento_ocupado():
                     historial_ventas = historial_ventas + 1
+                    historial_total_ventas = historial_total_ventas + 1
                 
                 decision = input("¿Desea reservar otro asiento? (s/n): ").lower()
-                if decision != "s" or decision != "si":
+                if decision != "s" and decision != "si":
+                    print("Volviendo al menú principal...")
                     ciclo = True
 
     elif opcion == 3:
@@ -148,8 +169,10 @@ while not menu:
             print("Ingrese el asiento que desea devolver")
             ocupados = sum(fila.count("X") for fila in matriz_asientos)
 
+            # Para evitar errores, se valida que existan asientos ocupados antes de intentar devolver un asiento. Si no hay asientos ocupados, el programa termina el ciclo.
             if ocupados <= 0:
                 print("No hay localidades ocupadas para devolver.")
+                print("Volviendo al menú principal...")
                 ciclo2 = True
 
             else:
@@ -158,12 +181,14 @@ while not menu:
                     historial_ventas = historial_ventas - 1
 
                 decision = input("¿Desea devolver otro asiento? (s/n): ").lower()
-                if decision != "s" or decision != "si":
+                if decision != "s" and decision != "si":
+                    print("Volviendo al menú principal...")
                     ciclo2 = True
     
     elif opcion == 4:
         print(f"Total de ventas netas realizadas durante la sesión: {historial_ventas}")
         print(f"Total de devoluciones realizadas durante la sesión: {devoluciones}")
+        print(f"Total de ventas realizadas, sin descontar por devoluciones: {historial_total_ventas}")
     
     elif opcion == 5:
         print("Gracias por utilizar nuestro software, hasta la próxima.")
