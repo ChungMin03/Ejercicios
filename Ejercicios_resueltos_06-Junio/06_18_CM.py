5cbe80fcd0d9604e2dcdd0d435060600bb2c5d10
Carlos Muñoz <caa.munozp@duocuc.cl>
2026-06-18 15:49:09 -0400

    Update 06_18_CM.py

---
+++ Ejercicios_resueltos_06-Junio/06_18_CM.py
@@ -0,0 +1,12 @@
+#-------------------------------- BLOQUE DE FUNCIONES -------------------------------------------
+
+#funcion que muestra el menu solicitado
+def mostrarMenu():
+    print("-------------------- MENU PRINCIPAL ---------------------")
+    print("1. .")
+    print("2. .")
+    print("3. .")
+    print("4. .")
+    print("5. .")
+    print("6. Salir.")
+    print("---------------------------------------------------------")
